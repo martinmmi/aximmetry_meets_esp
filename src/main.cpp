3ae085bb7f25e66618ac927234e2deb05ad97616
martinmmi <martin.mittrenga@t-online.de>
2023-07-05 15:06:06 +0200

    change

--- src/main.cpp
+++ src/main.cpp
@@ -1,56 +1,155 @@
-#include <WebServer.h>
-#include <WiFi.h>
-#include <WiFiUdp.h>
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
 
-//set up to connect to an existing network (e.g. mobile hotspot from laptop that will run the python code)
-const char* ssid = "ssid";
-const char* password = "password";
-WiFiUDP Udp;
-unsigned int localUdpPort = 4210;  //  port to listen on
-char incomingPacket[255];  // buffer for incoming packets
+#include <Arduino.h>
+#include <ETH.h>
+#include <WiFi.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+#define LED_PIN_INTERNAL    25        //Accumulator Function Declaration
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+unsigned long delayTime = 0;
+
+float temperature;
+float pressure;
+float altitude;
+float humidity;
+
+int boottime;
+
+char buf_boottime[20];
+char buf_temperature[20];
+char buf_pressure[20];
+char buf_humidity[20];
+
+//The udp library class
+WiFiUDP udp;
+
+//set up to connect to an existing network
+const char* ssid = "AirDoLan";
+const char* password = "mmg1199#";
+
+//const char* ssid = "Karli68";
+//const char* password = "xxxxx";
+
+//IP address to send UDP data to:
+const char *udpAddress = "192.168.1.75";
+const int udpPort = 3333;
+
+Adafruit_BME280 bme; // I2C
+
+//////////////////////////////////////////////////////////////////////
+
+void initWiFi() {
+  Serial.println("Setup WiFi");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+  Serial.println("Setup done");
+}
+
+//////////////////////////////////////////////////////////////////////
 
 void setup()
 {
-  int status = WL_IDLE_STATUS;
-  Serial.begin(115200);
-  WiFi.begin(ssid, password);
-  Serial.println("");
 
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("Connected to wifi");
-  Udp.begin(localUdpPort);
-  Serial.printf("Now listening at IP %s, UDP port %d\n", WiFi.localIP().toString().c_str(), localUdpPort);
+  Serial.begin(115200);                   // initialize serial
+  while (!Serial);
 
-  // we recv one packet from the remote so we can know its IP and port
-  bool readPacket = false;
-  while (!readPacket) {
-    int packetSize = Udp.parsePacket();
-    if (packetSize)
-     {
-      // receive incoming UDP packets
-      Serial.printf("Received %d bytes from %s, port %d\n", packetSize, Udp.remoteIP().toString().c_str(), Udp.remotePort());
-      int len = Udp.read(incomingPacket, 255);
-      if (len > 0)
-      {
-        incomingPacket[len] = 0;
-      }
-      Serial.printf("UDP packet contents: %s\n", incomingPacket);
-      readPacket = true;
-    } 
+  Serial.println("UDP Sender starts");
+  
+  Serial.println("BME280 Selftest!");
+
+  bool bme_status = bme.begin(0x76); 
+
+  if (!bme_status) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
   }
+
+  pinMode(LED_PIN_INTERNAL, OUTPUT);
+
+  initWiFi();
+
 }
+
+//////////////////////////////////////////////////////////////////////
 
 void loop()
-{
-  // once we know where we got the inital packet from, send data back to that IP address and port
-  Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
-  // Just test touch pin - Touch0 is T0 which is on GPIO 4.
-  Udp.printf(String(touchRead(T0)).c_str(),2);
-  Udp.endPacket();
-  delay(1000);
+{ 
+
+  if ((millis() - delayTime > 5000)) {
+    
+    temperature = bme.readTemperature() - 1;
+    int temperature_int = (int) temperature;
+    float temperature_float = (abs(temperature) - abs(temperature_int)) * 100;
+    int temperature_fra = (int)temperature_float;
+    sprintf (buf_temperature, "%d.%d", temperature_int, temperature_fra);
+
+
+    pressure = bme.readPressure() / 100.0F;
+    int pressure_int = (int) pressure;
+    float pressure_float = (abs(pressure) - abs(pressure_int)) * 100;
+    int pressure_fra = (int)pressure_float;
+    sprintf (buf_pressure, "%d.%d", pressure_int, pressure_fra);
+
+
+    humidity = bme.readHumidity();
+    int humidity_int = (int) humidity;
+    float humidity_float = (abs(humidity) - abs(humidity_int)) * 100;
+    int humidity_fra = (int)humidity_float;
+    sprintf (buf_humidity, "%d.%d", humidity_int, humidity_fra);
+
+
+    boottime = millis() / 1000;
+    sprintf (buf_boottime, "%d", boottime);
+
+
+    if (WiFi.status() == WL_CONNECTED){
+      //Send a packet
+
+      Serial.println("Sends UDP Packet:");
+
+      udp.beginPacket(udpAddress, udpPort);
+
+      Serial.print("Seconds since boot: "); Serial.print(boottime); Serial.print(" Temperature: "); Serial.print(temperature); Serial.print(" C "); Serial.print(" Pressure: "); Serial.print(pressure); Serial.print(" hPa "); Serial.print(" Humidity: "); Serial.print(humidity); Serial.println(" %");
+      
+      udp.printf("Seconds since boot: ");
+      udp.printf(buf_boottime);
+      udp.printf(" Temperature: ");
+      udp.printf(buf_temperature);
+      udp.printf(" C");
+      udp.printf(" Pressure: ");
+      udp.printf(buf_pressure);
+      udp.printf(" hPa");
+      udp.printf(" Humidity: ");
+      udp.printf(buf_humidity);
+      udp.printf(" %");
+      udp.endPacket();
+      
+      digitalWrite(LED_PIN_INTERNAL, HIGH);
+      delay(50);
+      digitalWrite(LED_PIN_INTERNAL, LOW);
+    }
+
+    if (WiFi.status() != WL_CONNECTED){
+      Serial.println("Something goes wrong with the wifi :/");
+    }
+
+    delayTime = millis();
+  }
+
 }
 
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
